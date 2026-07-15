079c12a5b6a1eb10db0752fa2d65bda10be63e9e
deradiv <abim4nyu25@gmail.com>
2026-07-15 13:29:20 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+window.DASHBOARD_CONFIG = {
+  organizationName: "BPS KABUPATEN NABIRE",
+  dataMode: "google-sheet",
+  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSvVtl-0x_TjmpTtgLLwgB0pW26mqX-KR9v-0va5OeFnnuiQ4HWUMzOpPYBFO6Te0mQVYSPoZKOFaIv/pub?gid=698571820&single=true&output=csv",
+  itemsPerPage: 6,
+  pageIntervalMs: 12000,
+  scheduleRefreshIntervalMs: 60000,
+  tickerText: "Selamat bekerja. Pastikan seluruh agenda, penugasan, rapat, monitoring, cuti, dan kegiatan kantor telah diperbarui melalui Google Sheets.",
+  weatherLatitude: -3.3670,
+  weatherLongitude: 135.4830,
+  weatherLocationName: "Nabire, Papua Tengah",
+  weatherTimezone: "Asia/Jayapura",
+  weatherRefreshIntervalMs: 600000
+};
